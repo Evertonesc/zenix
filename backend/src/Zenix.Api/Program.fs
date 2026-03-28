683011cd357b0169e9e88ddc3547e8179ef0fe57
esc <evertonsouzaesc@gmail.com>
2026-03-28 16:48:49 -0300

    chore: add giraffe web framework package and remove built in classes and controllers

--- backend/src/Zenix.Api/Program.fs
+++ backend/src/Zenix.Api/Program.fs
@@ -2,27 +2,35 @@ namespace Zenix.Api
 
 #nowarn "20"
 
+open Giraffe
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
 
 module Program =
     let exitCode = 0
+    
+    let webApp =
+        choose [
+            route "/ping" >=> text "pong"
+        ]
+        
+    let configureApp (app: IApplicationBuilder) =
+        app.UseGiraffe webApp
+        
+    let configureServices(services: IServiceCollection) =
+        services.AddGiraffe() |> ignore
 
     [<EntryPoint>]
     let main args =
-
-        let builder = WebApplication.CreateBuilder(args)
-
-        builder.Services.AddControllers()
-
-        let app = builder.Build()
-
-        app.UseHttpsRedirection()
-
-        app.UseAuthorization()
-        app.MapControllers()
-
-        app.Run()
-
+        Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(
+                fun webHostBuilder ->
+                    webHostBuilder
+                        .Configure(configureApp)
+                        .ConfigureServices(configureServices)
+                        |> ignore)
+            .Build()
+            .Run()
         exitCode
