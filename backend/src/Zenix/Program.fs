296ea5e8b200ca35ae7d3b9ff8483c66e5f04195
esc <evertonsouzaesc@gmail.com>
2026-03-28 22:31:48 -0300

    chore: rename fsproj

---
+++ backend/src/Zenix/Program.fs
@@ -0,0 +1,30 @@
+namespace Zenix
+
+#nowarn "20"
+
+open Giraffe
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+module Program =
+    let exitCode = 0
+
+    let webApp = choose [ route "/ping" >=> text "pong" ]
+
+    let configureApp (app: IApplicationBuilder) = app.UseGiraffe webApp
+
+    let configureServices (services: IServiceCollection) = services.AddGiraffe() |> ignore
+
+    [<EntryPoint>]
+    let main _ =
+        Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder.Configure(configureApp).ConfigureServices(configureServices)
+                |> ignore)
+            .Build()
+            .Run()
+
+        exitCode
